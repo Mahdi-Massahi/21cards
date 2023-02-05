a37fa7a2042716abfc2039d6d14b1ff0dd485f9e
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2023-02-05 13:12:43 +0330

    21 game cards implemented

---
+++ twenty_one_cards.py
@@ -0,0 +1,57 @@
+"""
+This script prepares and generates the cards for the 21 game.
+"""
+import itertools
+
+import cards
+
+from random import shuffle
+
+
+class TwentyOne:
+
+    def __init__(self, p_number_of_decks: int=1, do_shuffle: bool=True) -> None:
+        self.cards = []
+
+        the_ace = cards.Symbols.ACE
+        usual_cards = [e.value for e in cards.Symbols][1:10]  # cards from 2 to 10
+
+        # generate cards and set the points for the game
+        for deck, suit, symbol in itertools.product(
+            range(p_number_of_decks), 
+            cards.Suits, 
+            cards.Symbols):
+
+            # generate the card
+            card = cards.Card(
+                p_suit=suit,
+                p_symbol=symbol,
+                p_deck_number=deck,
+            )
+
+            # set the point of the card
+
+            if card.symbol is the_ace:
+                card.set_point(
+                    p_value=11, 
+                    p_alternative_value=1,
+                )
+
+            elif card.symbol in usual_cards:
+                card.set_point(p_value=int(symbol.value))
+
+            elif card.symbol is cards.Symbols.JACK:
+                card.set_point(p_value=1)
+
+            elif card.symbol is cards.Symbols.QUEEN:
+                card.set_point(p_value=2)
+
+            elif card.symbol is cards.Symbols.KING:
+                card.set_point(p_value=3)
+            
+            # append tha card
+            self.cards.append(card)
+
+        # shuffle the cards
+        if do_shuffle:
+            shuffle(self.cards)
